3952342c7a082c98f68c3b8502d3faa86fab1183
trung <trungtdtu@gmail.com>
2019-11-10 17:09:49 +0700

    them file moi

---
+++ Doan/js/controller.js
@@ -0,0 +1,10 @@
+var app = angular.module('DoanCNPM',[])
+.controller("khongcogi",function($scope){
+
+});
+// .directive("headHtml",function(){
+// 	return{
+// 		templateUrl: "../view/head.html"
+// 	}
+// })
+
